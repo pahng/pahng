795686b2c306306fcdc2741542534f3826a40196
Mikael Dunhem <mikael.dunhem@gmail.com>
2017-12-17 14:09:06 -0500

    Add comment blocks to dropdown component

--- src/app/shared/dropdown/dropdown.component.ts
+++ src/app/shared/dropdown/dropdown.component.ts
@@ -15,34 +15,83 @@ import {
 })
 export class DropdownComponent implements OnInit {
 
+  /**
+   * Whether the dropdown is active or not, which will set the `active` CSS class when true.
+   *
+   * @public
+   * @type {boolean}
+   */
   @HostBinding('class.active') isActive = false;
 
+  /**
+   * The text used in the dropdown button
+   *
+   * @public
+   * @type {string}
+   */
   @Input() dropdownText = 'Username';
 
+  /**
+   * Reference to the dropdown menu DOM element
+   *
+   * @public
+   * @type {HTMLElement}
+   */
   @ViewChild('menu', { read: ElementRef }) menu: ElementRef;
 
+  /**
+   * Constructor
+   *
+   * @param {ElementRef} element - Reference to the DOM element of this component
+   */
   constructor(private element: ElementRef) {
     this.element.nativeElement.classList.add('ui');
     this.element.nativeElement.classList.add('dropdown');
     this.element.nativeElement.classList.add('item');
   }
 
+  /**
+   * OnInit lifecycle hook
+   *
+   * @public
+   * @return void
+   */
   ngOnInit() {
   }
 
+  /**
+   * Click event handler on the dropdown to open and close it.
+   *
+   * @private
+   * @return void
+   */
   @HostListener('click')
-  private _clickHandler() {
+  private _clickHandler(): void {
     this._toggleDropdown();
   }
 
+  /**
+   * Click handler on the document, which is used to close the dropdown
+   * if the user clicks anywhere outside of the dropdown.
+   *
+   * @private
+   * @param {Event} event - The click event
+   * @return void
+   */
   @HostListener('document:click', ['$event'])
-  private _clickedOutside($event) {
-    if (!this.element.nativeElement.contains($event.target) && this.isActive) {
+  private _clickedOutside(event: Event): void {
+    if (!this.element.nativeElement.contains(event.target) && this.isActive) {
       this._toggleDropdown();
     }
   }
 
-  private _toggleDropdown() {
+  /**
+   * Opens or closes the dropdown.
+   *
+   * @private
+   * @return void
+   */
+  private _toggleDropdown(): void {
     this.menu.nativeElement.classList.toggle('visible');
     this.isActive = !this.isActive;
   }
