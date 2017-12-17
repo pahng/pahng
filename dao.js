4c70a55f1b6fc0d1419561ec046c5df0ec25cb6e
rossmas <ross@Ross-PC.localdomain>
2017-12-17 05:50:27 -0500

    more data routes

--- dao.js
+++ dao.js
@@ -9,9 +9,31 @@ var client = new pg.Client({
 });
 client.connect();
 
+
 module.exports = {
     getUsers: function(req, res) {    
         var query = client.query("SELECT * FROM users", function(err, results, fields) {
+            if (err) throw err;
+            res.send(results.rows);
+        });
+    },
+
+    getTeams: function(req, res) {    
+        var query = client.query("SELECT * FROM teams", function(err, results, fields) {
+            if (err) throw err;
+            res.send(results.rows);
+        });
+    },
+    
+    getSinglesGames: function(req, res) {    
+        var query = client.query("SELECT * FROM singles_games", function(err, results, fields) {
+            if (err) throw err;
+            res.send(results.rows);
+        });
+    },
+
+    getDoublesGames: function(req, res) {    
+        var query = client.query("SELECT * FROM doubles_games", function(err, results, fields) {
             if (err) throw err;
             res.send(results.rows);
         });
