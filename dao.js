38d0a361710bfad5ba01a4440114352d3ac88ab6
rossmas <ross@Ross-PC.localdomain>
2017-12-17 11:19:20 -0500

    var to const

--- dao.js
+++ dao.js
@@ -1,5 +1,5 @@
-var pg = require('pg');
-var client = new pg.Client({
+const pg = require('pg');
+const client = new pg.Client({
     user: "lzndoxqpefxjsw",
     password: "86c5e658992d95f16fd9eebaf49de10fac4820ffcf36d8e004e1611d6676478a",
     database: "d3hrvgu0u1qf2l",
@@ -12,28 +12,28 @@ client.connect();
 
 module.exports = {
     getUsers: function(req, res) {    
-        var query = client.query("SELECT * FROM users", function(err, results, fields) {
+        const query = client.query("SELECT * FROM users", function(err, results, fields) {
             if (err) throw err;
             res.send(results.rows);
         });
     },
 
     getTeams: function(req, res) {    
-        var query = client.query("SELECT * FROM teams", function(err, results, fields) {
+        const query = client.query("SELECT * FROM teams", function(err, results, fields) {
             if (err) throw err;
             res.send(results.rows);
         });
     },
     
     getSinglesGames: function(req, res) {    
-        var query = client.query("SELECT * FROM singles_games", function(err, results, fields) {
+        const query = client.query("SELECT * FROM singles_games", function(err, results, fields) {
             if (err) throw err;
             res.send(results.rows);
         });
     },
 
     getDoublesGames: function(req, res) {    
-        var query = client.query("SELECT * FROM doubles_games", function(err, results, fields) {
+        const query = client.query("SELECT * FROM doubles_games", function(err, results, fields) {
             if (err) throw err;
             res.send(results.rows);
         });
