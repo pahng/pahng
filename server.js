01d87b98d60658c97d0124f5efa727e4dba42f04
rossmas <ross@Ross-PC.localdomain>
2017-12-17 01:45:27 -0500

    routes

--- server.js
+++ server.js
@@ -22,12 +22,12 @@ app.use(forceSSL());
 
 // Run the app by serving the static files
 // in the dist directory
-app.use(express.static(__dirname + '/../dist'));
+app.use(express.static(__dirname + 'dist'));
 
 // For all GET requests, send back index.html
 // so that PathLocationStrategy can be used
-app.get('/*', function(req, res) {
-  res.sendFile(path.join(__dirname + '/../dist/index.html'));
+app.get('*', function(req, res) {
+  res.sendFile(path.join(__dirname + 'dist/index.html'));
 });
 
 // Start the app by listening on the default
