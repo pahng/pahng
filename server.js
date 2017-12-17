ff1d2d4eed2ab3b010989f1d563c864bf509bea3
rossmas <ross@Ross-PC.localdomain>
2017-12-17 01:48:28 -0500

    routes

--- server.js
+++ server.js
@@ -15,21 +15,18 @@ const forceSSL = function() {
   }
 }
 
-// Instruct the app
-// to use the forceSSL
-// middleware
+// Instruct the app to use the forceSSL middleware
 app.use(forceSSL());
 
 // Run the app by serving the static files
 // in the dist directory
-app.use(express.static(__dirname + 'dist'));
+app.use(express.static(__dirname + '/dist'));
 
 // For all GET requests, send back index.html
 // so that PathLocationStrategy can be used
 app.get('*', function(req, res) {
-  res.sendFile(path.join(__dirname + 'dist/index.html'));
+  res.sendFile(path.join(__dirname + '/dist/index.html'));
 });
 
-// Start the app by listening on the default
-// Heroku port
+// Start the app by listening on the default Heroku port
 app.listen(process.env.PORT || 8080);
