7f522c60d1c1bf746f6f531caa1724972ff47ea7
Mikael Dunhem <mikael.dunhem@gmail.com>
2017-12-18 23:27:38 -0500

    Remove unused import

--- src/public/app/features/users/users.module.ts
+++ src/public/app/features/users/users.module.ts
@@ -3,7 +3,6 @@ import { CommonModule } from '@angular/common';
 
 import { UsersRoutingModule } from './users-routing.module';
 import { UsersComponent } from './users.component';
-import { CoreModule } from '../../core/core.module';
 
 @NgModule({
   imports: [
